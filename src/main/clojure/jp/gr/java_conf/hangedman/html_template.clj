29615b515f370873e3f2f0e2fb94a5ca11a1b331
hiroyuki nagata <newserver002@gmail.com>
2018-04-02 00:37:29 +0900

    Implement load template and parse it

--- src/main/clojure/jp/gr/java_conf/hangedman/html_template.clj
+++ src/main/clojure/jp/gr/java_conf/hangedman/html_template.clj
@@ -26,11 +26,34 @@
   [this key]
   (@(.state this) key))
 
+(defn stream [input]
+  (new ANTLRInputStream input))
+
+(defn html-parser [tokens]
+  (new HTMLParser tokens))
+
+(defn html-lexer [stream]
+  (new HTMLLexer stream))
+
+(defn tokens [lexer]
+  (new CommonTokenStream lexer))
+
+(defn parse [elements]
+  (let [stream (stream elements)
+        lexer (html-lexer stream)
+        tok (tokens lexer)
+        parser (html-parser tok)
+        ctx (.htmlDocument parser)]
+    (debug (.toStringTree ctx))))
+
 ;;
 ;; ctor
 ;;
 (defnk -init [filename {option nil} {path nil} {utf8 nil}]
   (debug (str "TEMPLATE: \n" (str filename)))
+  (let [contents (slurp (io/resource filename))]
+    (debug contents)
+    (parse contents))
   [[] (atom {})])
 
 ;;
@@ -50,25 +73,3 @@
   "")
 
 ;; -------------------------------------------------------------------------------
-
-(defn stream [input]
-  (new ANTLRInputStream input))
-
-(defn html-parser [tokens]
-  (new HTMLParser tokens))
-
-(defn html-lexer [stream]
-  (new HTMLLexer stream))
-
-(defn tokens [lexer]
-  (new CommonTokenStream lexer))
-
-;; (defn -main
-;;   "I don't do a whole lot ... yet."
-;;   [& args]
-;;   (let [stream (stream "<html>Hello,World</html>")
-;;         lexer (html-lexer stream)
-;;         tok (tokens lexer)
-;;         parser (html-parser tok)
-;;         ctx (.htmlDocument parser)]
-;;     (println (.toStringTree ctx))))
